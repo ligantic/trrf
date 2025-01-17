84ee505b639e175ad38396d06af55e50a3d0574e
ppettitau <paola.pettit@qut.edu.au>
2025-01-17 11:58:23 +1000

    [ERP-533] Delete unused parent registration forms and classes from base TRRF. These are not currently used by any hosted registries.

--- rdrf/rdrf/settings.py
+++ rdrf/rdrf/settings.py
@@ -685,8 +685,7 @@ REGISTRATION_FORM = "rdrf.forms.registration_forms.PatientRegistrationForm"
 REGISTRATION_CLASS = "registry.groups.registration.patient.PatientRegistration"
 
 # Parent Registration (also adding a patient at registration time)
-# REGISTRATION_FORM = "rdrf.forms.registration_forms.ParentWithPatientRegistrationForm"
-# REGISTRATION_CLASS = "registry.groups.registration.parent_with_patient.ParentWithPatientRegistration"
+# [ERP-533] Deleted these forms and classes as they are unused across all currently hosted registries.
 
 # In case you set up customised email templates for the "new-patient" notification, you should
 # set this to False, otherwise the default registration email will also be sent to the user.
