de4e5fa85c3578a1f70c66f2c44f9b3ffab0ad50
ppettitau <paola.pettit@qut.edu.au>
2025-01-22 13:45:29 +1000

    Refactor to use a single tag

--- rdrf/rdrf/templatetags/password_reset_timeout.py
+++ rdrf/rdrf/templatetags/password_reset_timeout.py
@@ -1,23 +1,15 @@
-from datetime import datetime, timedelta, timezone
+from datetime import timedelta
 
 from django import template
 from django.conf import settings
 from django.template.defaultfilters import timeuntil
+from django.utils import timezone
 
 register = template.Library()
 
 
 @register.simple_tag
-def password_reset_timeout_timedelta():
-    return timedelta(seconds=settings.PASSWORD_RESET_TIMEOUT)
-
-
-@register.simple_tag
-def now():
-    return datetime.now(timezone.utc)
-
-
-@register.simple_tag
-def timeuntil_expiry(from_datetime, expiry_timedelta):
-    expiry_datetime = from_datetime + expiry_timedelta
-    return timeuntil(expiry_datetime, from_datetime)
+def timeuntil_expiry():
+    now = timezone.now()
+    expiry_timedelta = timedelta(seconds=settings.PASSWORD_RESET_TIMEOUT)
+    return timeuntil(now + expiry_timedelta, now)
