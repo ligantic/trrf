235ae8229d9b1ef8348fb3353f57007daec3e29e
ppettitau <paola.pettit@qut.edu.au>
2025-01-22 11:33:28 +1000

    [ERP-531] Use django utils for human readable time strings instead of custom methods for proper implementation of i18n.

--- rdrf/rdrf/templatetags/password_reset_timeout.py
+++ rdrf/rdrf/templatetags/password_reset_timeout.py
@@ -1,45 +1,23 @@
-from dateutil.relativedelta import relativedelta
+from datetime import datetime, timedelta, timezone
+
 from django import template
 from django.conf import settings
-from django.utils.translation import gettext as _
+from django.template.defaultfilters import timeuntil
 
 register = template.Library()
 
 
-def _pluralize(count: int, singular: str, plural: str):
-    return f"{count} {plural if count != 1 else singular}"
-
-
-def _readable_timedelta(delta):
-    parts = []
-
-    if delta.years > 0:
-        parts.append(_pluralize(delta.years, _("year"), _("years")))
-
-    if delta.months > 0:
-        parts.append(_pluralize(delta.months, _("month"), _("months")))
-
-    if delta.days > 0:
-        parts.append(_pluralize(delta.days, _("day"), _("days")))
-
-    # Don't go too granular if we already have a value for a bigger unit of time
-    if not parts:
-        if delta.hours > 0:
-            parts.append(_pluralize(delta.hours, _("hour"), _("hours")))
-
-        if delta.minutes > 0:
-            parts.append(_pluralize(delta.minutes, _("minute"), _("minutes")))
-
-    return ", ".join(parts)
+@register.simple_tag
+def password_reset_timeout_timedelta():
+    return timedelta(seconds=settings.PASSWORD_RESET_TIMEOUT)
 
 
 @register.simple_tag
-def password_reset_timeout():
-    timeout = settings.PASSWORD_RESET_TIMEOUT
+def now():
+    return datetime.now(timezone.utc)
 
-    if timeout is None:
-        return None
 
-    relative_timeout = relativedelta(seconds=timeout)
-
-    return _readable_timedelta(relative_timeout)
+@register.filter
+def timeuntil_expiry(from_datetime, expiry_timedelta):
+    expiry_datetime = from_datetime + expiry_timedelta
+    return timeuntil(expiry_datetime, from_datetime)
