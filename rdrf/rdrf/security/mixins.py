caee6c4e57601670a27859390e6eeddd85f20e92
ppettitau <paola.pettit@qut.edu.au>
2025-03-14 10:46:56 +1000

    [ERP-536] remove logging statements

--- rdrf/rdrf/security/mixins.py
+++ rdrf/rdrf/security/mixins.py
@@ -31,10 +31,6 @@ class TokenAuthenticatedMixin(AccessMixin):
         is_valid_token, username = check_token(
             self.username_b64, self.token, self.max_age
         )
-        import logging
-
-        logger = logging.getLogger(__name__)
-        logger.info("TODO - token authentication mixin")
         self.user = get_object_or_404(
             CustomUser, username=username, is_active=True, is_locked=False
         )
