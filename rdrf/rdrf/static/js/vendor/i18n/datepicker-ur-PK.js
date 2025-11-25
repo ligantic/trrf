5f94c8159e87ac32409ba6057fd9eec0f05a8cc5
Phil Clarke <phil@ligantic.com>
2025-11-25 14:11:18 +1100

    feat: Add Urdu (ur/ur-PK) datepicker i18n support

---
+++ rdrf/rdrf/static/js/vendor/i18n/datepicker-ur-PK.js
@@ -0,0 +1,41 @@
+/* Urdu (Pakistan) Translation for jQuery UI date picker plugin. */
+/* Written for Angelman Syndrome Registry */
+
+( function( factory ) {
+	"use strict";
+
+	if ( typeof define === "function" && define.amd ) {
+
+		// AMD. Register as an anonymous module.
+		define( [ "../widgets/datepicker" ], factory );
+	} else {
+
+		// Browser globals
+		factory( jQuery.datepicker );
+	}
+} )( function( datepicker ) {
+"use strict";
+
+datepicker.regional[ "ur-PK" ] = {
+	closeText: "بند کریں",
+	prevText: "&#x3C;پچھلا",
+	nextText: "اگلا&#x3E;",
+	currentText: "آج",
+	monthNames: [ "جنوری", "فروری", "مارچ", "اپریل", "مئی", "جون",
+	"جولائی", "اگست", "ستمبر", "اکتوبر", "نومبر", "دسمبر" ],
+	monthNamesShort: [ "جنوری", "فروری", "مارچ", "اپریل", "مئی", "جون",
+	"جولائی", "اگست", "ستمبر", "اکتوبر", "نومبر", "دسمبر" ],
+	dayNames: [ "اتوار", "پیر", "منگل", "بدھ", "جمعرات", "جمعہ", "ہفتہ" ],
+	dayNamesShort: [ "اتوار", "پیر", "منگل", "بدھ", "جمعرات", "جمعہ", "ہفتہ" ],
+	dayNamesMin: [ "اتوار", "پیر", "منگل", "بدھ", "جمعرات", "جمعہ", "ہفتہ" ],
+	weekHeader: "ہفتہ",
+	dateFormat: "dd/mm/yy",
+	firstDay: 0,
+	isRTL: true,
+	showMonthAfterYear: false,
+	yearSuffix: "" };
+datepicker.setDefaults( datepicker.regional[ "ur-PK" ] );
+
+return datepicker.regional[ "ur-PK" ];
+
+} );
