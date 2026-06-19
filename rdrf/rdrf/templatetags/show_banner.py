3036b36223e715436ae94887bfbccb6c883894f9
Mark Cola <mark@ligantic.com>
2026-06-19 13:39:46 +1000

    add support for custom banner text

--- rdrf/rdrf/templatetags/show_banner.py
+++ rdrf/rdrf/templatetags/show_banner.py
@@ -7,3 +7,8 @@ register = template.Library()
 @register.simple_tag
 def show_banner():
     return settings.SHOW_BANNER
+
+
+@register.simple_tag
+def banner_text():
+    return getattr(settings, "BANNER_TEXT", "")
